7d53843437f60fa9e5a1c5cc8cb4da5ceb5c0050
joseph <josephjsph69@hotmail.com>
2017-04-16 03:45:40 -0300

    ok Programado

---
+++ ApplicationJoseph/app/src/main/java/com/example/applicationjoseph/ActivityPrincipal.java
@@ -0,0 +1,22 @@
+package com.example.applicationjoseph;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ActivityPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+
+    //chamar tela de editar texto
+    public void OnClcEdtText (View view)
+    {
+        Intent intencaoTelaEditText = new Intent(getApplication(),TelaEditText.class);
+        startActivity(intencaoTelaEditText);
+    }
+}
--- ApplicationJoseph/app/src/main/java/com/example/applicationjoseph/TelaEditText.java
+++ ApplicationJoseph/app/src/main/java/com/example/applicationjoseph/TelaEditText.java
@@ -1,21 +1,37 @@
 package com.example.applicationjoseph;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class TelaEditText extends AppCompatActivity {
+public class TelaEditText extends AppCompatActivity
+{
 
-
-
+    private Button enviarAlert;
+    private EditText texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_edit_text);
 
+        enviarAlert = (Button) findViewById(R.id.btnEnviarTexto);
+        texto = (EditText) findViewById(R.id.editText1);
+
+        enviarAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(TelaEditText.this);
+                dlg.setMessage(texto.getText().toString());
+                dlg.setNeutralButton("Clique em OK", null);
+                dlg.show();
+            }
+        });
 
 
     }
